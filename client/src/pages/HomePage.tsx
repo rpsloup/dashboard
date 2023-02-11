20288f6b2994bc227add6434e7d3a95a0e8075e9
rpsloup <rpsloup@proton.me>
2023-02-11 21:10:38 +0100

    FE: Fetching the daily tasks from the BE

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,23 @@
 import DefaultLayout from '../layouts/DefaultLayout';
+import { useEndpoint } from '../hooks/fetchHooks';
 
-const HomePage = (): JSX.Element => (
-  <DefaultLayout>
-    <h1>Home</h1>
-  </DefaultLayout>
-);
+const HomePage = (): JSX.Element => {
+  const { data: dailyTasks } = useEndpoint('/daily');
+
+  return (
+    <DefaultLayout>
+      <h1>Home</h1>
+      <ul>
+        {dailyTasks
+          ? (dailyTasks as any[]).map((task) => (
+              <li key={task.task_id}>
+                {task.title} ({task.time})
+              </li>
+            ))
+          : null}
+      </ul>
+    </DefaultLayout>
+  );
+};
 
 export default HomePage;
